8f5c7a6704157df55275c03c5b61a8ef25c1a8d4
Muhammad Rivki <muhammad.rivki@live.com>
2020-10-20 02:24:23 +0700

    feat: add transition health app ios

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
 import '../styles/index.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function App({ Component, pageProps }) {
+  return (
+    <div className="bg-white min-h-screen">
+      <div className="min-container container max-w-lg mx-auto bg-black opacity-75 relative overflow-hidden">
+        <Component {...pageProps} />
+      </div>
+    </div>
+  )
 }
 
-export default MyApp
+export default App
--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,69 @@
-import { motion } from 'framer-motion'
+import { AnimatePresence, motion } from 'framer-motion'
+import { useState } from 'react'
+
+const offcanvas = {
+  hidden: { y: '100%', opacity: 0 },
+  show: { y: 0, opacity: 1, transition: { duration: 0.3, type: 'just' } },
+  exit: { y: '100%', opacity: 1, transition: { duration: 0.3, type: 'just' } },
+}
+
+const container = {
+  hidden: { opacity: 0 },
+  show: {
+    opacity: 1, scale: 1, borderRadius: '0px', transition: { duration: 0.5 },
+  },
+  zoomOut: {
+    scale: 0.95, y: -10, borderRadius: '15px', transition: { duration: 0.3 },
+  },
+}
+
+const overlay = {
+  hidden: { opacity: 0, transition: { duration: 0.15 } },
+  show: { opacity: 0.75, transition: { duration: 0.2, delay: 0.15 } },
+}
 
 export default function Home() {
+  const [active, setActive] = useState(false)
   return (
-    <div
-      className="flex items-center justify-center min-h-screen overflow-hidden  bg-gradient-to-r from-purple-400 via-pink-500 to-red-500"
-    >
+    <>
       <motion.div
-        className="bg-white h-40 w-40"
-        animate={{
-          scale: [1, 2, 2, 1, 1],
-          rotate: [0, 0, 270, 270, 0],
-          borderRadius: ['20%', '20%', '50%', '50%', '20%'],
-        }}
-        transition={{
-          duration: 2,
-          ease: 'easeInOut',
-          times: [0, 0.2, 0.5, 0.8, 1],
-          loop: Infinity,
-          repeatDelay: 1,
-        }}
+        variants={container}
+        initial="hidden"
+        animate={active ? 'zoomOut' : 'show'}
+        className="flex items-center justify-center min-h-screen overflow-hidden bg-gray-100"
       >
-        &nbsp;
+        <motion.button
+          type="button"
+          className="py-2 px-5 bg-blue-500 text-white rounded-lg"
+          whileTap={{ scale: 0.9 }}
+          onTap={() => setActive(!active)}
+        >
+          Click Me
+        </motion.button>
       </motion.div>
-    </div>
+      <motion.div
+        initial="hidden"
+        animate={active ? 'show' : 'hidden'}
+        variants={overlay}
+        style={{ pointerEvents: 'auto' }}
+        className="z-0 fixed w-screen h-screen bg-black opacity-75 top-0 left-0"
+        onClick={() => setActive(!active)}
+      >
+        <span className="w-full h-full absolute top-0 left-0">&nbsp;</span>
+      </motion.div>
+      <AnimatePresence>
+        {active && (
+          <motion.div
+            variants={offcanvas}
+            initial="hidden"
+            animate="show"
+            exit="exit"
+            className="overlay-container w-full bg-white absolute rounded-t-3xl shadow-md bottom-0"
+          >
+            Hello
+          </motion.div>
+        ) }
+      </AnimatePresence>
+    </>
   )
 }
